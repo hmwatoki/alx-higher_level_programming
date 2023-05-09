5e848b5573737dbe2bc15415270dfbbfa1997ebe
hmwatoki <harrisonmwatoki@gmail.com>
2023-05-09 21:01:25 +0300

    fix: Corrected typo in 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for c in str:
         ascii_cd = ord(c)
         if ascii_cd >= 97 and ascii_cd <= 122:
-            uppercase_code = ascii_cd - 32
-            c = chr(uppercase_code)
+            uppercase_cde = ascii_cd - 32
+            c = chr(uppercase_cde)
         print("{}".format(c), end='')
     print()
