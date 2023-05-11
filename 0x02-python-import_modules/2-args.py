43d35e7658ad09a77c0b2dc4ce34206ba045abaf
hmwatoki <harrisonmwatoki@gmail.com>
2023-05-11 14:26:59 +0300

    fix: Add logic to print arguments in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 import sys
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    x = 1
 
-args = sys.argv[1:]
-x = 1
+    print("{} arguments:".format(len(args)))
 
-print("{} arguments:".format(len(args)))
-
-for arg in args:
-    print("{}: {}".format(x, arg))
-    x += 1
+    for arg in args:
+        print("{}: {}".format(x, arg))
+        x += 1
