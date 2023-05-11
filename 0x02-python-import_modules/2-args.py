65a8159caef24844642ea32bb9f2a9c369881075
hmwatoki <harrisonmwatoki@gmail.com>
2023-05-11 14:37:29 +0300

    fix: Update 2-args.py with improved argument handling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
 import sys
 if __name__ == "__main__":
+    arglen = len(sys.argv) - 1
     args = sys.argv[1:]
-    x = 1
-
-    print("{} arguments:".format(len(args)))
-
-    for arg in args:
-        print("{}: {}".format(x, arg))
-        x += 1
+    plural = "s" if arglen > 1 else ""
+    print("{:d} argument{:s}{}"
+          .format(arglen, plural, ":" if arglen else "."))
+    for i, arg in enumerate(args):
+        print("{:d}: {}".format(i + 1, arg))
