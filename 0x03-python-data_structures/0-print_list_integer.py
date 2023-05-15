a993c5ba30b179d2be8ef128acd27d75c7315b19
hmwatoki <harrisonmwatoki@gmail.com>
2023-05-15 10:46:05 +0300

    feat: Added 0-print_list_integer.py to print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,30 +1,4 @@
-# Welcome to my Python project on Data Structures: Lists, Tuples
-
-This is my project for the ALX SE bootcamp on Python Data Structures: Lists, Tuples. I'll be learning you how to use these structures to store and manipulate data like a pro. But before we dive in, let me introduce myself.
-
-My name is Harry, and I'm a Python enthusiast. I love using this language. It's really cool :)
-
-## What are Lists and Tuples?
-
-Lists and tuples are two of the most commonly used data structures in Python. They allow you to store a collection of items in a single variable, which you can then access and manipulate as needed. Here's a quick rundown of each structure:
-
-- **Lists**: A list is a collection of items that are ordered and changeable. You can add, remove, or modify items in a list, and you can access individual items by their index.
-- **Tuples**: A tuple is similar to a list, but it's immutable, which means you can't change the items after you create it. Tuples are often used to store related pieces of information that shouldn't be changed, like the coordinates of a point on a map.
-
-## How to Use Lists and Tuples
-
-Now that you know what lists and tuples are, let's dive into how to use them in Python. Here are some common operations you might perform with these structures:
-
-- **Creating a list or tuple**: To create a list or tuple, you simply put the items inside square brackets (for a list) or parentheses (for a tuple), separated by commas. For example:
-'python mylist = [1, 2, 3, 4, 5] mytuple = ("apple", "banana", "cherry")'
-
-
-- **Accessing items in a list or tuple**: To access an item in a list or tuple, you use its index. Keep in mind that Python uses zero-based indexing, which means the first item in the list or tuple is at index 0. For example:
-'python mylist = [1, 2, 3, 4, 5] firstitem = mylist[0]' # This will be 1 lastitem = my_list[-1] # This will be 5
-
-
-- **Adding items to a list**: To add an item to the end of a list, you can use the `append()` method. For example:
-'python mylist = [1, 2, 3, 4, 5] mylist.append(6)' # This will add 6 to the end of the list
-
-- **Removing items from a list**: To remove an item from a list, you can use the `remove()` method. For example:
-'python mylist = [1, 2, 3, 4, 5] mylist.remove(3)'
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{:d}".format(num))
