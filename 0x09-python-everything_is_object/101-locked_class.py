ffd0cd46fe16213ecd768de20cdc7e6cf91e9660
hmwatoki <hmwatoki@gmail.com>
2023-06-07 04:03:00 +0300

    fix: Update AttributeError message in LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,5 +2,5 @@
 class LockedClass:
     def __setattr__(self, key, value):
         if key != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(key))
         self.__dict__[key] = value
