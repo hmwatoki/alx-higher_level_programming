bab4b5aadba4749a5a14ce617e86411f368a604e
hmwatoki <harrisonmwatoki@gmail.com>
2023-06-08 14:43:21 +0300

    fix: Add documentation and tests for 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+This module contains functions for performing addition.
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Adds two numbers together.
+
+    Args:
+        a: A number.
+        b: second number.
+
+    Returns:
+        The sum of a and b.
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an interger or float")
     if type(b) not in [int, float]:
