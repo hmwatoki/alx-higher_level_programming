fefd3f141c50c050b806cd9fd44bcbbbcc602cbd
hmwatoki <hmwatoki@gmail.com>
2023-06-13 13:57:57 +0300

    fix: Update read_file function for improved readability

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Defines function read_file"""
+
+
 def read_file(filename=""):
     """
-    A function that reads the contents of a file and prints it to the console.
+    A function that reads the contents of a file
 
     Args:
-        filename (str): The name or path of the file to read. Defaults to an empty string.
+        filename(str):The name or path of the file to read.
 
     Returns:
         None
     """
-    with open (filename, mode='r', encoding='utf-8') as file:
+    with open(filename, mode='r', encoding='utf-8') as file:
         content = file.read()
         print(content)
