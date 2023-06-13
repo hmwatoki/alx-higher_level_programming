d2f88a4aed991fb761741d98acfeecf71e6564cf
hmwatoki <hmwatoki@gmail.com>
2023-06-13 04:07:10 +0300

    feat: Added 1-my_list.py to 0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function to print a sorted list"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """
+        Sorts the elements in the object and prints them in ascending order.
+
+        :param self: The object to be sorted and printed.
+        :return: None
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
