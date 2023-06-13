568795d9d6de87f8ec047acc3ffaf3f00fccdf6a
hmwatoki <harrisonmwatoki@gmail.com>
2023-06-13 16:13:26 +0300

    feat: Added 3-to_json_string.py and 3-main.py files

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+def to_json_string(my_obj):
+    """
+    Converts a Python object into a JSON string representation.
+
+    Args:
+        my_obj (Any): The Python object to be converted.
+
+    Returns:
+        str: The JSON string representation of the given object.
+    """
+    json_str = json.dumps(my_obj)
+    return json_str
