1573f2afbd2ec0d141eee09a93686d370928e480
hmwatoki <hmwatoki@gmail.com>
2023-06-13 04:40:23 +0300

    fix: reverse equality for MyInt class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Defines class MyInt"""
+"""defines myInt class"""
 
 
 class MyInt(int):
-    def __eq__(self, other):
-        """
-        Check if the current object is equal to another object by comparing it with the other object using the '!=' operator.
+    """instance of MyInt"""
 
-        :param other: The other object to compare this object to.
-        :type other: any
-        :return: True if the current object is not equal to the other object, False otherwise.
-        :rtype: bool
-        """
-        return super().__ne__(other)
+    def __init__(self, value):
+        """initializes class"""
+        self.value = value
 
-    def __ne__(self, other):
-        """
-        This function checks if the object is not equal to another object by utilizing the
-        `__eq__` method of the parent class.
+    def __eq__(self, b):
+        """reverse equals to (not equals)"""
+        return self.value != b
 
-        :param self: The object that the method is called upon.
-        :param other: The object to compare with.
-        :return: The result of the comparison between the two objects.
-        :rtype: bool
-        """
-        return super().__eq__(other)
+    def __ne__(self, b):
+        """reverses not (equals to equals)"""
+        return self.value == b
