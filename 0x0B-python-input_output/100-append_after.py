2cf3f82a5ca9f9a5f0917a47af6dd2c533435980
hmwatoki <hmwatoki@gmail.com>
2023-06-14 05:15:08 +0300

    fix: Update description in 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""inserts a line of text to a file, after each line containing a specific string"""
+"""inserts a line of text to a file, after each line"""
 
 
 def append_after(filename="", search_string="", new_string=""):
