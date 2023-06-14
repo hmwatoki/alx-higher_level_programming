f32353e7032b34b56b4cbfca504ebb24ddb45ac8
hmwatoki <hmwatoki@gmail.com>
2023-06-14 05:42:29 +0300

    fix: Update main() to parse line and print metrics every 10 lines

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,53 +1,71 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics"""
+
+"""
+Reads input from stdin line by line, parses each line, updates the 
+total size and status codes, and prints metrics every 10 lines.
+"""
+
+import sys
 
 
-def print_stats(size, status_codes):
-    """Print metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
+def print_metrics(total_size, status_codes):
     """
-    status_codes = dict(sorted(status_codes.items()))
-    print(f"File size: {size}")
-    print("\n".join([f"{key}: {status_codes[key]}" for key in status_codes]))
-    
+    Prints the total file size and counts of each status code.
 
-if __name__ == "__main__":
-    import sys
+    :param total_size: An integer representing the total size of the file.
+    :param status_codes: A dictionary containing the counts of each status code.
+    :type total_size: int
+    :type status_codes: dict
+    """
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] != 0:
+            print("{}: {}".format(code, status_codes[code]))
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
 
+def parse_line(line, total_size, status_codes):
+    """
+    Parses a single line of log data to extract the status code and size.
+
+    :param line: A string representing a single line of log data.
+    :param total_size: The current total size of log data processed.
+    :param status_codes: A dictionary containing the count of each status code encountered.
+    :type line: str
+    :type total_size: int
+    :type status_codes: dict
+
+    :return: A tuple containing the updated total size and status code count dictionary.
+    :rtype: Tuple[int, dict]
+    """
+    try:
+        fields = line.split()
+        size = int(fields[-1])
+        code = fields[-2]
+        total_size += size
+        if code in status_codes:
+            status_codes[code] += 1
+    except Exception:
+        pass
+    return total_size, status_codes
+
+
+def main():
+    """
+    Reads input from stdin line by line, parses each line, updates the 
+    total size and status codes, and prints metrics every 10 lines.
+    """
+    i = 0
+    total_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     try:
         for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
+            i += 1
+            total_size, status_codes = parse_line(line, total_size, status_codes)
+            if i % 10 == 0:
+                print_metrics(total_size, status_codes)
     except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+        print_metrics(total_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
