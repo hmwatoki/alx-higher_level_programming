0d5176499e888c9c8e86651c48fe59de11b1dd2c
hmwatoki <hmwatoki@gmail.com>
2023-06-14 04:58:22 +0300

    fix: Import correct modules and update logic for adding items to list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,16 @@ Adds all arguments to a Python list, and then saves them to a file.
 """
 import sys
 import json
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-try:
-    jsonList = load_from_json_file("add_item.json")
-except:
-    jsonList = []
 
-for i in sys.argv[1:]:
-    jsonList.append(i)
-save_to_json_file(jsonList, "add_item.json")
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
