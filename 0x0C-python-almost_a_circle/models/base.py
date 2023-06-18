1d2cec1c5a7d672f92267230c6490a9c8aa36a5a
hmwatoki <hmwatoki@gmail.com>
2023-06-18 05:19:01 +0300

    feat: Added Base class and 0-main.py to almost_a_circle directory

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a base class"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes the `Base` class by assigning the attributes
+        Args:
+            id (int, optional): An integer value representing
+            the `id` attribute.
+            Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
