a4b19a3f614c9c0da0cd2d855617436dd0cf2bf4
hmwatoki <hmwatoki@gmail.com>
2023-06-20 09:06:15 +0300

    fix: Update Rectangle class setters and getters with new properties

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,98 +9,99 @@ class Rectangle(Base):
     """
     Rectangle class that inherits from Base class.
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         Initializes the Rectangle class
         with width, height, x, y, and id attributes.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     # Getter methods
-    def get_width(self):
+    @property
+    def width(self):
         """
         Returns the width attribute of the Rectangle object.
         """
         return self.__width
 
-    def get_height(self):
+    @property
+    def height(self):
         """
         Returns the height attribute of the Rectangle object.
         """
         return self.__height
 
-    def get_x(self):
+    @property
+    def x(self):
         """
-        Returns the x attribute of the Rectangle object.
+       Returns the x attribute of the Rectangle object.
         """
         return self.__x
 
-    def get_y(self):
+    @property
+    def y(self):
         """
-        Returns the y attribute of the Rectangle object.
-        """
+         Returns the y attribute of the Rectangle object.
+         """
         return self.__y
 
     # Setter methods
-    def set_width(self, value):
-        """
-        Sets the width attribute of the Rectangle object to the given value.
-        """
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
-    def set_height(self, value):
-        """
-        Sets the height attribute of the Rectangle object to the given value.
-        """
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
-    def set_x(self, value):
-        """
-        Sets the x attribute of the Rectangle object to the given value.
-        """
+    @x.setter
+    def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
         else:
             self.__x = value
 
-    def set_y(self, value):
-        """
-        Sets the y attribute of the Rectangle object to the given value.
-        """
+    @y.setter
+    def y(self, value):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
 
     def area(self):
         """returns area"""
-        return self.__width * self.__height
+        return self.width * self.height
 
     def display(self):
         """Displays rectangle"""
-        for i in range(self.__height):
-            print('#' * self.__width)
+        for i in range(self.height):
+            print('#' * self.width)
 
     def __str__(self):
         """Overrides default string representation"""
         return '[Rectangle] ({}) {}/{} - {}/{}'.format(
             self.id,
-            self.get_x(),
-            self.get_y(),
-            self.get_width(),
-            self.get_height()
+            self.x,
+            self.y,
+            self.width,
+            self.height
         )
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Rectangle test module"""
+
+"""
+Rectangle test module
+"""
+
 import unittest
 from io import StringIO
 from models.rectangle import Rectangle
@@ -7,86 +11,122 @@ from unittest.mock import patch
 
 
 class TestRectangle(unittest.TestCase):
-    """Tests for the Rectangle class"""
+    """
+    Tests for the Rectangle class
+    """
 
     def setUp(self):
         """
         Initializes the test fixture with a rectangle object.
-
-        :param self: The test fixture.
-        :return: None.
         """
-
         self.rectangle = Rectangle(10, 20, 30, 40, 1)
 
     def test_get_width(self):
-        """Test width getter"""
-        self.assertEqual(self.rectangle.get_width(), 10)
+        """
+        Test width getter
+        """
+        self.assertEqual(self.rectangle.width, 10)
 
     def test_get_height(self):
-        """Test height getter"""
-        self.assertEqual(self.rectangle.get_height(), 20)
+        """
+        Test height getter
+        """
+        self.assertEqual(self.rectangle.height, 20)
 
     def test_get_x(self):
-        """Test x value getter"""
-        self.assertEqual(self.rectangle.get_x(), 30)
+        """
+        Test x value getter
+        """
+        self.assertEqual(self.rectangle.x, 30)
 
     def test_get_y(self):
-        """Test y value getter"""
-        self.assertEqual(self.rectangle.get_y(), 40)
+        """
+        Test y value getter
+        """
+        self.assertEqual(self.rectangle.y, 40)
 
     def test_set_width(self):
-        """Test width getter"""
-        self.rectangle.set_width(15)
-        self.assertEqual(self.rectangle.get_width(), 15)
+        """
+        Test width setter
+        """
+        self.rectangle.width = 15
+        self.assertEqual(self.rectangle.width, 15)
 
     def test_set_width_raises_error(self):
-        """Test width with error"""
+        """
+        Test width with error
+        """
         with self.assertRaises(TypeError):
-            self.rectangle.set_width("15")
+            self.rectangle.width = "15"
 
     def test_set_height(self):
-        """Test height getter"""
-        self.rectangle.set_height(25)
-        self.assertEqual(self.rectangle.get_height(), 25)
+        """
+        Test height setter
+        """
+        self.rectangle.height = 25
+        self.assertEqual(self.rectangle.height, 25)
 
     def test_set_height_raises_error(self):
-        """Test height with error"""
+        """
+        Test height with error
+        """
         with self.assertRaises(TypeError):
-            self.rectangle.set_height("25")
+            self.rectangle.height = "25"
 
     def test_set_x(self):
-        """Test x value getter"""
-        self.rectangle.set_x(35)
-        self.assertEqual(self.rectangle.get_x(), 35)
+        """
+        Test x value setter
+        """
+        self.rectangle.x = 35
+        self.assertEqual(self.rectangle.x, 35)
 
-    def test_set_x_raises_error(self):
-        """Test x with error"""
+    def test_set_x_raises_error_type_error(self):
+        """
+        Test x with TypeError error
+        """
         with self.assertRaises(TypeError):
-            self.rectangle.set_x("35")
+            self.rectangle.x = "35"
+
+    def test_set_x_raises_error_value_error(self):
+        """
+        Test x with ValueError error
+        """
         with self.assertRaises(ValueError):
-            self.rectangle.set_x(-5)
+            self.rectangle.x = -5
 
     def test_set_y(self):
-        """Test y value getter"""
-        self.rectangle.set_y(45)
-        self.assertEqual(self.rectangle.get_y(), 45)
+        """
+        Test y value setter
+        """
+        self.rectangle.y = 45
+        self.assertEqual(self.rectangle.y, 45)
 
-    def test_set_y_raises_error(self):
-        """Test y with error"""
+    def test_set_y_raises_error_type_error(self):
+        """
+        Test y with TypeError error
+        """
         with self.assertRaises(TypeError):
-            self.rectangle.set_y("45")
+            self.rectangle.y = "45"
+
+    def test_set_y_raises_error_value_error(self):
+        """
+        Test y with ValueError error
+        """
         with self.assertRaises(ValueError):
-            self.rectangle.set_y(-5)
+            self.rectangle.y = -5
 
     def test_area(self):
-        """Tests area"""
+        """
+        Tests area
+        """
         r = Rectangle(4, 7, 1, 4)
         expected_area = 4 * 7
         self.assertEqual(r.area(), expected_area)
 
     def test_display(self):
-        """Tests display"""
+        """
+        Tests display
+        """
         r = Rectangle(3, 2)
         with patch('sys.stdout', new=StringIO()) as buffer:
             r.display()
@@ -94,7 +134,9 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(output, '###\n###')
 
     def test_str(self):
-        """Tests __str__ method"""
+        """
+        Tests __str__ method
+        """
         r = Rectangle(7, 4, 1, 3)
         output = str(r)
         self.assertEqual(output, '[Rectangle] ({}) 1/3 - 7/4'.format(r.id))
