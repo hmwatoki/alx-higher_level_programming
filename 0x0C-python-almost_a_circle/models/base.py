ccc6da40c1d0e895f7c1c99b191a2c42649a55f3
hmwatoki <hmwatoki@gmail.com>
2023-06-24 04:24:56 +0300

    feat: Added save_to_file classmethod to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,16 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                dict_list = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(dict_list))
