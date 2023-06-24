6ba6d1eb8ab9d799faf892e61bc18f79203fd9f0
hmwatoki <hmwatoki@gmail.com>
2023-06-24 05:09:59 +0300

    fix: Update Base class to_json_string and save_to_file methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 """Defines a base class"""
-
 import json
 
 
@@ -11,7 +9,7 @@ class Base:
 
     def __init__(self, id=None):
         """
-        Initializes the `Base` class by assigning the attributes.
+        Initializes the `Base` class by assigning the attributes
         Args:
             id (int, optional): An integer value representing
             the `id` attribute.
@@ -32,40 +30,38 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-
+   
     @classmethod
     def save_to_file(cls, list_objs):
         """
         Writes the JSON string representation of list_objs to a file.
-
+        
         The filename is <Class name>.json and overwrites any existing file.
-
+        
         If list_objs is None, saves an empty list.
-
+        
         Args:
             list_objs (list): A list of instances that inherit from Base.
 
         Returns:
             Nothing. Writes to a file instead.
 
-        """
+       """
         filename = cls.__name__ + ".json"
-
+        
         if list_objs is None:
-            with open(filename, mode='w', encoding='utf-8') as f:
-                f.write("[]")
-                return
-
-        # Convert objects to dictionaries and then to JSON string
+           with open(filename, mode='w', encoding='utf-8') as f:
+               f.write("[]")
+               return
 
         list_dict = []
         for obj in list_objs:
-            obj_dict = obj.to_dictionary()
-            list_dict.append(obj_dict)
-
+           obj_dict = obj.to_dictionary()
+           list_dict.append(obj_dict)
+           
         json_str = cls.to_json_string(list_dict)
-
-        # Write JSON string to file
-
+       
+       # Write JSON string to file
+       
         with open(filename, mode='w', encoding='utf-8') as f:
-            f.write(json_str)
+           f.write(json_str)
