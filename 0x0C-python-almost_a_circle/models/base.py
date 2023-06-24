4b54177e37695ec7ed3270d8b111d4d170552342
hmwatoki <hmwatoki@gmail.com>
2023-06-24 03:46:46 +0300

    feat: Added to_json_string method to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base class"""
+import json
 
 
 class Base:
@@ -19,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+
+        if list_dictionaries is not None or len(list_dictionaries) > 0:
+            return json.dumps(list_dictionaries)
+        else:
+            return "[]"
